bc8188827a074f807804239ec55a98c35e183365
zengruoyu <zengruoyu@qingtingfm.com>
2015-11-13 18:29:00 +0800

    [rtsp] py3k support

--- lib/rtspparser.py
+++ lib/rtspparser.py
@@ -1,3 +1,4 @@
+from .utils import convert_bytes_to_str
 from .rtspcommand import RTSPCommand
 
 
@@ -8,16 +9,19 @@ class RTSPParser(object):
 
     def rtsp_parse_packet(self, packet):
         """Parses the RTSP packet."""
-        method = packet.read_bytes(4)
+        method = convert_bytes_to_str(packet.read_bytes(4))
         if method != 'PLAY':
             return None
-        data = packet.get_bytes(packet.size).split('\r\n')
+        data = convert_bytes_to_str(packet.get_bytes(packet.size))
+        if not data:
+            return
+        data = data.split('\r\n')
 
         rtsp_command = RTSPCommand()
         rtsp_command.name = 'PLAY'
         rtsp_command.args['url'] = data[0][data[0].find(' ') + 1:
                                            data[0].rfind(' ')]
         if '://' not in rtsp_command.args['url']:
-            return None
+            return
 
         return rtsp_command
